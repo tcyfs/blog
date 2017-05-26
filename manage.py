c5098f191b9b21a9714f4f4a0f8b38142de25ee9
tcyfs <15745890592qq.com>
2017-05-26 17:18:33 +0800

    delete database

--- manage.py
+++ manage.py
@@ -37,6 +37,7 @@ def deploy():
     from flask_migrate import upgrade
     from app.models import Role, User
     # 把数据库迁移到最新修订版本
+    drop_all()
     upgrade()
     # 创建用户角色
     Role.insert_roles()
